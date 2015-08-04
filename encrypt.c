2607488d5e6c39b3848efeee397221e06192732d
Mark <tuc72616@temple.edu>
2015-08-04 10:23:30 -0400

    strings and such

---
+++ encrypt.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    char input[256];
+    char output[206];
+    char *middle;
+    
+    
+        scanf("%s", input);
+//   input = input + "\0"; 
+    if (sizeof(input)>sizeof(output)) {
+    middle= (char *) malloc(sizeof(input));
+    strcpy(middle, input);
+
+    for(int i; i<350; i=i+1){    
+    printf("%s"," \n done\n");
+    }
+
+    
+    strcpy(output,input);
+    printf("%s",strcat(middle,  "\n"));
+
+    printf("%s",output);
+    }
+}
